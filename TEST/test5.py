949cb9b55c4b2b898458b4d90a10d5824534a22b
blinKiss <blinkiss@naver.com>
2023-03-13 09:12:01 +0900

    13

--- TEST/test5.py
+++ TEST/test5.py
@@ -12,31 +12,39 @@ font = font_manager.FontProperties(fname=font_path).get_name()
 rc('font', family=font)
 
 df = pd.read_csv('./EyEDi/data/한국교통안전공단_자동차제작결함신고정보_20211231.csv')
-df_slice = df['제작사'].value_counts()
-# print(df_slice)
+# print(df[df['제작사'].isnull()])
 
-h, k, s, r, g = 0, 0, 0, 0, 0
+# 수정 전 코드
+# h, k, s, r, g = 0, 0, 0, 0, 0
 
-for i in range(len(df_slice)):
-    if '현대' in df_slice.index[i]:
-        # company.append(df_slice.index[i])
-        h += df_slice[i]
-    if '기아' in df_slice.index[i]:
-        k += df_slice[i]
-    if '쌍용' in df_slice.index[i]:
-        s += df_slice[i]
-    if '르노' in df_slice.index[i]:
-        r += df_slice[i]
-    if '지엠' in df_slice.index[i]:
-        g += df_slice[i]
-# print(x, y)
+# for i in range(len(df_slice)):
+#     if '현대' in df_slice.index[i]:
+#         # company.append(df_slice.index[i])
+#         h += df_slice[i]
+#     if '기아' in df_slice.index[i]:
+#         k += df_slice[i]
+#     if '쌍용' in df_slice.index[i]:
+#         s += df_slice[i]
+#     if '르노' in df_slice.index[i]:
+#         r += df_slice[i]
+#     if '지엠' in df_slice.index[i]:
+#         g += df_slice[i]
+# # print(x, y)
+# company = ['현대', '기아', '쌍용', '르노', '지엠']
+# # print(h)
+# count = [h, k, s, r, g]
+# cc = dict(sorted(zip(company, count), key=lambda x: x[1], reverse=True))
+df2 = df[['제작사']].dropna()
+
 company = ['현대', '기아', '쌍용', '르노', '지엠']
-# print(h)
-count = [h, k, s, r, g]
-cc = dict(sorted(zip(company, count), key=lambda x: x[1], reverse=True))
-# print(cc)
-plt.title('제조사별 자동차 결함 신고 수', loc='left')
-plt.pie(cc.values(), labels=cc.keys(), autopct='%.1f%%', startangle=90, counterclock=False)
-plt.show()
+count = []
 
-# print(count)
+for i in company:
+    count.append(df2[df2['제작사'].str.contains(i)].shape[0])
+
+cc = dict(sorted(zip(company, count), key=lambda x: x[1], reverse=True))
+
+plt.title('제조사별 자동차 결함 신고 수', loc='left')
+explode = (0.1, 0, 0, 0, 0)
+plt.pie(cc.values(),explode=explode, labels=cc.keys(), autopct='%.1f%%', startangle=90, counterclock=False)
+plt.show()
